2daa2288807836512650249c47bb32be47cbf177
Abdelmalek <crispybatata@gmail.com>
2024-12-29 23:26:36 +0100

    Test

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ function App() {
       <div className="app">
         <Navbar2 />
         <main className="content">
-          <Section/>
           <Routes>
             <Route path="/" element={<Section />} />
             <Route path="/services" element={<ServicesSection setActiveComponent={setActiveComponent} />} />
