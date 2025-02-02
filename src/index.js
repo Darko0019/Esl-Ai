42ac055a245aaf1a1beadeaef1fa9afcc3ccc744
Abdelmalek <crispybatata@gmail.com>
2025-02-02 14:55:33 +0100

    Error 404 fix #5

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <React.StrictMode>
-    <Router>
+    <Router basename='/Esl-Ai'>
       <App/>
     </Router>
   </React.StrictMode>
